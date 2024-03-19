7ed6a1d748ff93c7117f2a08e355fb4860d24b88
smzuluaga01 <santimzuluaga01@hotmail.com>
2024-03-19 18:05:30 -0500

    Segunda versión -> banner implementado

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+
+window.onload = () => {
+    BluesoftBank.methods.setDate();
+}
+
+
 const BluesoftBank = {
     sections : {
         profile : document.getElementById('app-mainsection-profile'),
@@ -7,6 +13,7 @@ const BluesoftBank = {
     },
     elements : {
         loginApprovation : false,
+        date: document.getElementById('app_lateral_date'),
         buttonProfile : document.getElementById('app-lateral-profile'),
         buttonProducts : document.getElementById('app-lateral-products'),
         buttonTransactions : document.getElementById('app-lateral-transactions'),
@@ -14,28 +21,96 @@ const BluesoftBank = {
         buttonLogout : document.getElementById('app-lateral-logout')
     },
     methods : {
+        hideAllSections : function () {
+            BluesoftBank.sections.profile.style.display = 'none';
+            BluesoftBank.sections.products.style.display = 'none';
+            BluesoftBank.sections.transactions.style.display = 'none';
+            BluesoftBank.sections.documents.style.display = 'none';
+        },
+        setDate : function () {
+            date = new Date;
+            console.log(date);
 
+            BluesoftBank.elements.date.innerHTML = String(date).substring(0,21)
+
+            console.log( BluesoftBank.elements.date.innerHTML);
+        }
     }
 }
 
 
 BluesoftBank.elements.buttonProfile.addEventListener('click', () => {
-    alert("profile")
+    
+    BluesoftBank.methods.hideAllSections();
+    BluesoftBank.sections.profile.style.display = 'flex';
+    swal.fire({
+        title : 'Perfil',
+        icon : 'success',
+        toast: true,
+        showConfirmButton : false,
+        showCloseFutton : false,
+        timer : 800,
+    })
 })
 
 BluesoftBank.elements.buttonProducts.addEventListener('click', () => {
-    alert("products")
+    BluesoftBank.methods.hideAllSections();
+
+    BluesoftBank.sections.products.style.display = 'flex';
+    swal.fire({
+        title : 'Productos',
+        icon : 'success',
+        toast: true,
+        showConfirmButton : false,
+        showCloseFutton : false,
+        timer : 800,
+    })
 })
 
 BluesoftBank.elements.buttonTransactions.addEventListener('click', () => {
-    alert("transactions")
+
+    BluesoftBank.methods.hideAllSections();
+    BluesoftBank.sections.transactions.style.display = 'flex';
+    swal.fire({
+        title : 'Transacciones',
+        icon : 'success',
+        toast: true,
+        showConfirmButton : false,
+        showCloseFutton : false,
+        timer : 800,
+    })
 })
 
 BluesoftBank.elements.buttonDocuments.addEventListener('click', () => {
-    alert("documents")
+
+    BluesoftBank.methods.hideAllSections();
+    BluesoftBank.sections.documents.style.display = 'flex';
+    swal.fire({
+        title : 'Documento',
+        icon : 'success',
+        toast: true,
+        showConfirmButton : false,
+        showCloseFutton : false,
+        timer : 800,
+    })
 })
 
 BluesoftBank.elements.buttonLogout.addEventListener('click', () => {
-    BluesoftBank.elements.loginApprovation = false;
-    window.location.href="index.html"
+
+    swal.fire({
+        title : 'Cerrando Sesión',
+        icon : 'success',
+        toast: true,
+        showConfirmButton : false,
+        showCloseFutton : false,
+        timer : 800,
+        timerProgressBar:true
+        
+    })
+    
+    setTimeout(() => {
+        BluesoftBank.methods.hideAllSections();
+        BluesoftBank.elements.loginApprovation = false;
+        window.location.href="index.html"
+    },800)
 })
